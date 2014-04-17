f109efc765bdb06829d1f4ee2ab58506a3639ae2
Alexander Pokluda <apokluda@uwaterloo.ca>
2014-04-16 23:18:10 -0400

    Added cache contol header to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 s3cmd sync . s3://alexanderpokluda.ca \
+--add-header='Cache-Control:max-age=43200, public' \
 --exclude '.git/*' \
 --exclude 'deploy.sh' \
 --exclude 'README.md' \
 --exclude '.project' \
---exclude 'dev/*'
+--exclude 'dev/*' \
+--exclude '*~'
